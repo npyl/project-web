6bb725c3679081618af8da58837a3d023ddc4cfa
npyl <ulcheats@gmail.com>
2021-12-13 16:18:23 +0200

    Show Map with correct longitude & latitude

--- src/js/init.js
+++ src/js/init.js
@@ -1,29 +1,18 @@
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
-}
+var latitude;
+var longitude;
 
-function onSuccess(position) {
-    console.log("Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude);
-}
-
-function onError(error) {
-    switch(error.code)
+function getLocation(handler) 
+{
+    if (navigator.geolocation) 
     {
-        case error.PERMISSION_DENIED:
-            console.log("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            console.log("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            console.log("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            console.log("An unknown error occurred.");
-            break;
+        navigator.geolocation.getCurrentPosition((position) => {
+            handler(position.coords.latitude, position.coords.longitude, null);
+        }, (err) => {
+            handler(null, null, err);
+        });
+    } 
+    else 
+    {
+        handler(null, null, new Error('Geolocation not supported by the browser'));
     }
 }
